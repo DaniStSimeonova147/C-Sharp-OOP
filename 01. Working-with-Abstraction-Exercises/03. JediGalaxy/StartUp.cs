765e9b5bc1cd94268ece72214dc98baf140efaef
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-31 17:19:57 +0200

    Jedi Galaxy

---
+++ 01. Working-with-Abstraction-Exercises/03. JediGalaxy/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_JediGalaxy
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            Runner runner = new Runner();
+
+            runner.Run();
+        }
+    }
+}
