9e58036b02d9585aa0b5a7edfecfead653f20641
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-31 17:09:46 +0200

    Exercises 1 and 2

---
+++ 01. Working-with-Abstraction-Exercises/01. RawData/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace P01_RawData
+{
+    class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            Runner runner = new Runner();
+            runner.Run();
+        }
+    }
+}
---
+++ 01. Working-with-Abstraction-Exercises/02.CarsSalesman/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace P02_CarsSalesman
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Runner runner = new Runner();
+            runner.Run();
+        }
+    }
+
+}
