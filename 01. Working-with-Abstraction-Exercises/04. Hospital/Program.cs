85109122f665da0aacd6f4e1fbb041934c59708c
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-31 17:21:13 +0200

    Hospital

---
+++ 01. Working-with-Abstraction-Exercises/04. Hospital/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Hospital
+{
+    public class Program
+    {
+        public static void Main()
+        {
+           Runner runner = new Runner();
+           runner.Run();
+        }
+    }
+}
