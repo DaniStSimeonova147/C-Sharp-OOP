7c50b19c8c059073d2bd1c8e53a59195ab90a08e
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-02 18:30:38 +0200

    Animals

---
+++ 02. Inheritance-Exercises/06. Animals/Animal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+
+namespace Animals
+{
+   public class Animal : SoundProducable
+    {
+        private string name;
+        private int age;
+        private Gender gender;
+
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        private string Name
+        {
+             set
+            {
+               
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        private int Age
+        {
+            set
+            {
+                if (value <=0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        private string Gender
+        {
+            set
+            {
+                Gender gender;
+
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value) || !Enum.TryParse<Gender>(value, out gender))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.gender = gender;
+            }
+        }
+
+        public virtual string ProduceSound()
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine(this.GetType().Name);
+            result.AppendLine($"{this.name} {this.age} {this.gender.ToString()}");
+            result.AppendLine(this.ProduceSound());
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 02. Inheritance-Exercises/06. Animals/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            List<Animal> animals = new List<Animal>();
+
+            string currentAnimal = Console.ReadLine();
+
+            while (currentAnimal != "Beast!")
+            {
+                string[] input = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                string gender = input[2];
+
+                try
+                {
+                    switch (currentAnimal)
+                    {
+                        case "Cat":
+                            animals.Add(new Cats.Cat(name, age, gender));
+                            break;
+                        case "Kitten":
+                            animals.Add(new Cats.Kitten(name, age));
+                            break;
+                        case "Tomcat":
+                            animals.Add(new Cats.Tomcat(name, age));
+                            break;
+                        case "Dog":
+                            animals.Add(new Dogs.Dog(name, age, gender));
+                            break;
+                        case "Frog":
+                            animals.Add(new Frogs.Frog(name, age, gender));
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                catch (ArgumentException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+
+                currentAnimal = Console.ReadLine();
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(string.Join(" ", animal));
+            }
+        }
+    }
+}
