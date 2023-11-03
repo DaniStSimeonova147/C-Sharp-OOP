4fb08b0e5f93bcd1c32917f0acdeae5e82fa0ef9
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-03 19:46:24 +0200

    Add exceptions

---
+++ 03. Encapsulation-Exercises/05. FootballTeamGenerator/Exeptions/ExeptionMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace E05.FootballTeamGenerator.Exeptions
+{
+   public static class ExeptionMessages
+    {
+        public static string InvalidStatExeption =
+            "{0} should be between {1} and {2}.";
+
+        public static string EmptyNameExeption =
+            "A name should not be empty.";
+
+        public static string MissingPlayerExeption =
+            "Player {0} is not in {1} team.";
+
+        public static string MissingTeamExeption =
+            "Team {0} does not exist.";
+    }
+}
