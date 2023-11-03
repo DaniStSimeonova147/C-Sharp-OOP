4ffa5bbadea13a68da9a8a37ce2d59f44c26a6fd
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-03 19:40:59 +0200

    Exercises 1 and 2

---
+++ 03. Encapsulation-Exercises/01. ClassBoxData/Exeptions/ExeptionMessege.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace E01.ClassBoxData.Exeptions
+{
+   public static class ExeptionMessege
+    {
+        public static string lengthExeption = "Length cannot be zero or negative.";
+        public static string widthExeption = "Width cannot be zero or negative.";
+        public static string heightExeption = "Height cannot be zero or negative.";
+    }
+}
---
+++ 03. Encapsulation-Exercises/01. ClassBoxData/Models/Box.cs
@@ -0,0 +1,105 @@
+﻿using E01.ClassBoxData.Exeptions;
+
+using System;
+using System.Text;
+
+namespace E01.ClassBoxData.Models
+{
+   public class Box
+    {
+        //length, width and height
+
+        private double length;
+
+        private double width;
+
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExeptionMessege.lengthExeption);
+                }
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+           private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExeptionMessege.widthExeption);
+                }
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+           private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExeptionMessege.heightExeption);
+                }
+                this.height = value;
+            }
+        }
+
+
+        public double SurfaceArea()
+        {
+            double surfaceArea = 2 * this.Length * this.Width + LateralSurfaceArea();
+
+            return surfaceArea;
+        }
+        public double LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+
+            return lateralSurfaceArea;
+        }
+        public double Volume()
+        {
+            double volume = this.Length * this.Width * this.Height;
+
+            return volume;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"Surface Area - {SurfaceArea():f2}")
+                .AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}")
+                .AppendLine($"Volume - {Volume():f2}");
+           
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
