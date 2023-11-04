4b8995c4f8183e6fc1cd343b7f71361fa5a72f67
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-04 22:03:36 +0200

    Military Elite

---
+++ 04. Interfaces-And-Abstraction-Exercises/06. Military Elite/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using E06.Elite.Core;
+
+namespace E06.Elite
+{
+   public class StartUp
+    {
+      public  static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
