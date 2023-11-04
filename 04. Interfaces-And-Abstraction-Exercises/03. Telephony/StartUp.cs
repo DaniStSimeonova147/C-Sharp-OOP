54d8a3df0ca4833d82a84511bbf011ef1b472bfd
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-04 21:58:41 +0200

    Telephony

---
+++ 04. Interfaces-And-Abstraction-Exercises/03. Telephony/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using E03.Telephony.Core;
+
+namespace E03.Telephony
+{
+   public class StartUp
+
+    {
+       public static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
