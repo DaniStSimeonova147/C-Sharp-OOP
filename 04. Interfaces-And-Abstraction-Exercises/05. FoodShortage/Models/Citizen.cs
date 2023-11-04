093dd24e280a267b82f0b4246b952522244b96e4
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-04 22:02:27 +0200

    Add models

---
+++ 04. Interfaces-And-Abstraction-Exercises/05. FoodShortage/Models/Citizen.cs
@@ -0,0 +1,31 @@
+﻿using E05.FoodShortage.Constraints;
+
+namespace E05.FoodShortage.Models
+{
+    public class Citizen : IBuyer
+    {
+        public Citizen(string name, int age, string id, string birthday)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birhtdate = birthday;
+            this.Food = 0;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Id { get; set; }
+
+        public string Birhtdate { get; set; }
+
+        public int Food { get; private set; }
+
+        public void BuyFood()
+        {
+            this.Food += 10;
+        }
+    }
+}
