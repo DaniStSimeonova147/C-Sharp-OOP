fd33e56eddccc63d2db1b96d2d8212d468e11843
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-04 21:59:28 +0200

    Add exceptions

---
+++ 04. Interfaces-And-Abstraction-Exercises/03. Telephony/Exceptions/InvalidNumberException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace E03.Telephony.Exceptions
+{
+    public class InvalidNumberException : Exception
+    {
+        private const string EXC_MESSAGE = "Invalid number!";
+        public InvalidNumberException()
+            : base(EXC_MESSAGE)
+        {
+
+        }
+        public InvalidNumberException(string message) 
+            : base(message)
+        {
+
+        }
+    }
+}
