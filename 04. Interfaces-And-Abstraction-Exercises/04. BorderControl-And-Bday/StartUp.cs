a6076740623b28b3c354d30dee4f214be4d25916
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-04 22:00:50 +0200

    Border control and birthday

---
+++ 04. Interfaces-And-Abstraction-Exercises/04. BorderControl-And-Bday/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using E04.BorderControl.Core;
+
+namespace E04.BorderControl
+{
+    public class StartUp
+    {
+      public  static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
