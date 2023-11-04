b92600378bc009e4092a4338951a0d726cdfe9e1
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-04 22:02:09 +0200

    Food shortage

---
+++ 04. Interfaces-And-Abstraction-Exercises/05. FoodShortage/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using E05.FoodShortage.Core;
+
+namespace E05.FoodShortage
+{
+  public  class StartUp
+    {
+      public  static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Run();
+        }
+    }
+}
