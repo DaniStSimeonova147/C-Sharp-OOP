e4fc5d6dac31a99a31490e7b1efa52cdeea5724d
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-04 21:59:03 +0200

    Add models

---
+++ 04. Interfaces-And-Abstraction-Exercises/03. Telephony/Models/Smartphone.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+using E03.Telephony.Constraints;
+using E03.Telephony.Exceptions;
+
+namespace E03.Telephony.Models
+{
+    public class Smartphone : ICal, IBrowse
+    {
+        public Smartphone()
+        {
+
+        }
+
+        public string Browse(string url)
+        {
+            if (url.Any(c => char.IsDigit(c)))
+            {
+                throw new InvalidUrlException();
+            }
+
+            return $"Browsing: {url}!";
+        }
+
+        public string Cal(string phoneNumber)
+        {
+            if (!phoneNumber.All(n => char.IsDigit(n)))
+            {
+                throw new InvalidNumberException();
+            }
+
+            return $"Calling... {phoneNumber}";
+        }
+    }
+}
