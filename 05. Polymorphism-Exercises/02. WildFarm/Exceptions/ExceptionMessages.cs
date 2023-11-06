584b797f54ae0c43be8118650c495c3ea9a2c438
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-06 19:47:30 +0200

    Add exceptions

---
+++ 05. Polymorphism-Exercises/02. WildFarm/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace E03.WildFarm.Exceptions
+{
+   public static class ExceptionMessages
+    {
+        public static string InvalidFoodTypeException =
+            "{0} does not eat {1}!";
+
+    }
+}
