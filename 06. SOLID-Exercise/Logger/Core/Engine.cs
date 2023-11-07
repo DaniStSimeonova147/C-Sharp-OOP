674020cf8b7dc211ae8c9aefcf8286cb5507aa13
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-07 14:59:30 +0200

    Add core

---
+++ 06. SOLID-Exercise/Logger/Core/Engine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Logger.Models.Factorys;
+using Logger.Models.Contracts;
+
+namespace Logger.Core
+{
+  public  class Engine
+    {
+        private ILogger logger;
+        private ErrorFactory errorFactory;
+
+        private Engine()
+        {
+            this.errorFactory = new ErrorFactory();
+        }
+        public Engine(ILogger logger)
+            :this()
+        {
+            this.logger = logger;
+        }
+
+        public void Run()
+        {
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] errorTokens = command.Split("|");
+
+                string level = errorTokens[0];
+                string date = errorTokens[1];
+                string message = errorTokens[2];
+
+                IError error;
+
+                try
+                {
+                    error = this.errorFactory.GetError(date, level, message);
+                    this.logger.Log(error);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(this.logger.ToString());
+        }
+    }
+}
