b9ea8c3bd0288695dbbbb68aaa691128ace0b32e
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-08 19:00:47 +0200

    Add Core

---
+++ 07. Reflection-and-Attributes-Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMAND_POSTFIX =
+            "Command";
+        public string Read(string inputLine)
+        {
+            string[] commandTokens = inputLine.Split(" ",
+                StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = commandTokens[0] + COMMAND_POSTFIX;
+
+            string[] commandArgs = commandTokens
+                .Skip(1)
+                .ToArray();
+
+            Assembly assembly = Assembly
+                .GetCallingAssembly();
+
+            Type[] types = assembly
+                .GetTypes();
+
+            Type typeToCreate = types
+                .FirstOrDefault(t => t.Name == commandName);
+
+            //if (typeToCreate == null)
+            //{
+            //    throw new InvalidOperationException
+            //        ("Invalid Command Type");
+            //}
+
+            Object instance =
+                Activator.CreateInstance(typeToCreate);
+
+            ICommand command = (ICommand)instance;
+
+            string result = command.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
