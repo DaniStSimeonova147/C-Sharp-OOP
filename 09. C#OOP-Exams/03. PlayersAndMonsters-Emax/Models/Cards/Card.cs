389aee91118526e9aa96966512bc0f0c2f31eaf0
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-14 09:14:24 +0200

    Add cards model

---
+++ 09. C#OOP-Exams/03. PlayersAndMonsters-Emax/Models/Cards/Card.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using PlayersAndMonsters.Common;
+using PlayersAndMonsters.Models.Cards.Contracts;
+
+namespace PlayersAndMonsters.Models.Cards
+{
+    public abstract class Card : ICard
+    {
+        private string name;
+        private int damagePoints;
+        private int healthPoints;
+
+        protected Card(string name, int damagePoints, int healthPoints)
+        {
+            this.Name = name;
+            this.DamagePoints = damagePoints;
+            this.HealthPoints = healthPoints;
+        }
+
+        public string Name
+        {
+            get => this.name;
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.NullCardName);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int DamagePoints
+        {
+            get => this.damagePoints;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.NegativeCardDamagePoints);
+                }
+
+                this.damagePoints = value;
+            }
+        }
+
+        public int HealthPoints
+        {
+            get => this.healthPoints;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.NegativeCardHealthPoints);
+                }
+
+                this.healthPoints = value;
+            }
+        }
+    }
+}
