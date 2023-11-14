687f83ac8cc2952a55ef070109f2162efea74091
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-11-14 09:08:05 +0200

    Add factories

---
+++ 09. C#OOP-Exams/03. PlayersAndMonsters-Emax/Core/Factories/CardFactory.cs
@@ -0,0 +1,42 @@
+﻿using PlayersAndMonsters.Core.Factories.Contracts;
+using PlayersAndMonsters.Models.Cards;
+using PlayersAndMonsters.Models.Cards.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace PlayersAndMonsters.Core.Factories
+{
+    public class CardFactory : ICardFactory
+    {
+        public ICard CreateCard(string type, string name)
+        {
+            ICard card = null;
+
+            switch (type)
+            {
+                case "Trap":
+                    card = new TrapCard(name);
+                    break;
+
+                case "Magic":
+                    card = new MagicCard(name);
+                    break;
+
+                default:
+                    break;
+            }
+
+            return card;
+            //Type cardType = Assembly.GetCallingAssembly()
+            //    .GetTypes()
+            //    .FirstOrDefault(t => t.Name == type);
+
+            //ICard card = (ICard)Activator.CreateInstance(cardType, name);
+
+            //return card;
+        }
+    }
+}
---
+++ 09. C#OOP-Exams/03. PlayersAndMonsters-Emax/Core/Factories/PlayerFactory.cs
@@ -0,0 +1,45 @@
+﻿using PlayersAndMonsters.Core.Factories.Contracts;
+using PlayersAndMonsters.Models.Players;
+using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace PlayersAndMonsters.Core.Factories
+{
+    public class PlayerFactory : IPlayerFactory
+    {
+        public IPlayer CreatePlayer(string type, string username)
+        {
+            IPlayer player = null;
+
+            switch (type)
+            {
+                case "Advanced":
+                    player = new Advanced(new CardRepository(), username);
+                    break;
+
+                case "Beginner":
+                    player = new Beginner(new CardRepository(), username);
+                    break;
+
+                default:
+                    break;
+            }
+
+            return player;
+
+            //Type playerType = Assembly.GetCallingAssembly()
+            //    .GetTypes()
+            //    .FirstOrDefault(t => t.Name == type);
+
+            //IPlayer player = (IPlayer)Activator.CreateInstance(playerType, username);
+
+            //return player;
+
+        }
+    }
+}
